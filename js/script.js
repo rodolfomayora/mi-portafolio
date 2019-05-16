7d03b8c7570ee14ff6dc3583be47916164b29a61
rodolfo <rodolfomayora96@gmail.com>
2019-05-15 21:36:00 -0400

    Doc: minify all docs .js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,1 @@
-var
-	burger = document.getElementById( 'burger' ),
-	body = document.getElementById( 'body' ),
-	navigation = document.getElementById( 'navigation' ),
-	content	= document.querySelectorAll( '.js-content' );
-
-burger.addEventListener( 'click', function () {
-
-	burger.classList.toggle( 'burger--active' );
-
-	navigation.classList.toggle( 'navigation--visible' );
-	
-	content[ 0 ].classList.toggle( 'blur' );
-	content[ 1 ].classList.toggle( 'blur' );
-	content[ 2 ].classList.toggle( 'blur' );
-
-	body.classList.toggle( 'scroll-off' );
-} )
+var burger=document.getElementById('burger'),body=document.getElementById('body'),navigation=document.getElementById('navigation'),content=document.querySelectorAll('.js-content');burger.addEventListener('click',function(){burger.classList.toggle('burger--active');navigation.classList.toggle('navigation--visible');content[0].classList.toggle('blur');content[1].classList.toggle('blur');content[2].classList.toggle('blur');body.classList.toggle('scroll-off');})
